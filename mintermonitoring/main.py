d1e13ab326ebe720b6dae9f72c10332ed85a5e06
Aidar Shaifutdinov <aidarshaifutdinov@gmail.com>
2019-09-23 15:05:47 +0300

    Get rid of duplicate alerts & add error handling

--- mintermonitoring/main.py
+++ mintermonitoring/main.py
@@ -9,86 +9,101 @@ from mintersdk.minterapi import MinterAPI
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print(u'Usage: {} config'.format(sys.argv[0]))
-        sys.exit(1)
-
-    # load config
-    with open(sys.argv[1], 'r') as f:
-        config = json.load(f)
-
-    # configure logging
-    logging.config.dictConfig(config['logger'])
-
-    updater = Updater(token=config['telegram_bot_token'], use_context=True)
-    dispatcher = updater.dispatcher
-
-    auth_key = config['monitoring_auth_key']
-    chats_file = config['telegram_chats_file']
-
     try:
-        with open(chats_file, 'rb') as handle:
-            chats = pickle.load(handle)
-    except FileNotFoundError:
-        chats = []  # [(bot, chat_id), ...]
+        if len(sys.argv) < 2:
+            raise Exception(u'Usage: {} config'.format(sys.argv[0]))
 
-    def start(update, context):
-        msg = update.message
-        bot = context.bot
-        chat_id = msg.chat_id
+        # load config
+        with open(sys.argv[1], 'r') as f:
+            config = json.load(f)
 
-        if (bot, chat_id) in chats:
-            bot.send_message(chat_id=chat_id, text="Already authorized")
-            return
+        # configure logging
+        logging.config.dictConfig(config['logger'])
 
-        msg_tokens = msg.text.split()
-        if len(msg_tokens) < 2 or msg_tokens[1] != auth_key:
-            bot.send_message(chat_id=chat_id, text="Auth failed")
-            return
+        updater = Updater(token=config['telegram_bot_token'], use_context=True)
+        dispatcher = updater.dispatcher
 
-        chats.append((bot, chat_id))
-        with open(chats_file, 'wb') as handle:
-            pickle.dump(chats, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        auth_key = config['monitoring_auth_key']
+        chats_file = config['telegram_chats_file']
 
-        bot.send_message(chat_id=chat_id, text="Successful auth")
+        try:
+            with open(chats_file, 'rb') as handle:
+                chats = pickle.load(handle)
+        except FileNotFoundError:
+            chats = []  # [(bot, chat_id), ...]
 
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
+        def start(update, context):
+            msg = update.message
+            bot = context.bot
+            chat_id = msg.chat_id
 
-    updater.start_polling()
+            if (bot, chat_id) in chats:
+                bot.send_message(chat_id=chat_id, text="Already authorized")
+                return
 
-    minterapi = MinterAPI(config['minter_api_url'])
-    pub_keys = config['minter_nodes_pub_keys']
+            msg_tokens = msg.text.split()
+            if len(msg_tokens) < 2 or msg_tokens[1] != auth_key:
+                bot.send_message(chat_id=chat_id, text="Auth failed")
+                return
 
-    while True:
-        for pub_key in pub_keys:
-            response = minterapi.get_candidate(pub_key)
+            chats.append((bot, chat_id))
+            with open(chats_file, 'wb') as handle:
+                pickle.dump(chats, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-            error = response.get('error')
-            if not error:
-                status = response['result']['status']
+            bot.send_message(chat_id=chat_id, text="Successful auth")
 
-                # If status = 1, node is offline
-                if status == 1:
-                    msg = '{} node is offline'.format(pub_key)
-                    logging.info(msg)
-                    for bot, chat_id in chats:
-                        bot.send_message(chat_id=chat_id, text=msg)
-            else:
-                logging.error('get_candidate: {}'.format(error))
+        start_handler = CommandHandler('start', start)
+        dispatcher.add_handler(start_handler)
 
-            response = minterapi.get_missed_blocks(pub_key)
+        updater.start_polling()
 
-            error = response.get('error')
-            if not error:
-                missed_blocks = int(response['result']['missed_blocks_count'])
+        minterapi = MinterAPI(config['minter_api_url'])
+        pub_keys = config['minter_nodes_pub_keys']
 
-                if missed_blocks > 0:
-                    msg = '{} node has missed {} blocks'.format(pub_key, missed_blocks)
-                    logging.info(msg)
-                    for bot, chat_id in chats:
-                        bot.send_message(chat_id=chat_id, text=msg)
-            else:
-                logging.error('get_missed_blocks: {}'.format(error))
+        nodes = {
+            pub_key: {'status': 2, 'missed_blocks': 0}
+            for pub_key in pub_keys
+        }
+        while True:
+            try:
+                for pub_key in pub_keys:
+                    node = nodes[pub_key]
 
-        time.sleep(1)
+                    response = minterapi.get_candidate(pub_key)
+
+                    error = response.get('error')
+                    if not error:
+                        status = response['result']['status']
+
+                        # If status = 1 and node was previously online, alert that it's offline now
+                        if status == 1 and node['status'] == 2:
+                            msg = '{} node is offline'.format(pub_key)
+                            logging.info(msg)
+                            for bot, chat_id in chats:
+                                bot.send_message(chat_id=chat_id, text=msg)
+                            node['status'] = 1
+                    else:
+                        logging.error('get_candidate: {}'.format(error))
+
+                    response = minterapi.get_missed_blocks(pub_key)
+
+                    error = response.get('error')
+                    if not error:
+                        missed_blocks = int(response['result']['missed_blocks_count'])
+
+                        # If missed blocks count has increased, alert
+                        if missed_blocks > node['missed_blocks']:
+                            msg = '{} node has missed {} blocks'.format(pub_key, missed_blocks)
+                            logging.info(msg)
+                            for bot, chat_id in chats:
+                                bot.send_message(chat_id=chat_id, text=msg)
+                        node['missed_blocks'] = missed_blocks
+                    else:
+                        logging.error('get_missed_blocks: {}'.format(error))
+            except Exception as e:
+                logging.error('{}: {}'.format(e.__class__.__name__, e.__str__()))
+
+            time.sleep(1)
+    except Exception as e:
+        logging.error('{}: {}'.format(e.__class__.__name__, e.__str__()))
+        sys.exit(1)
